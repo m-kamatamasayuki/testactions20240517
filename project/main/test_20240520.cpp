5258cc14e098720a60cf0b8f2869f2a73d4cab0b
m-kamatamasayuki <155719103+m-kamatamasayuki@users.noreply.github.com>
2024-05-20 16:22:20 +0900

    Create test_20240520.cpp

---
+++ project/main/test_20240520.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "number:";
+    std::cin >> num;
+    int squared = num * 2;
+    std::cout << "that number is " << squared << " is" << std::endl;
+    return 0;
+}
