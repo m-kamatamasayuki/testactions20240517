353f4a8c4eb525bb2b726c3587f3a7978b8c389b
m-kamatamasayuki <155719103+m-kamatamasayuki@users.noreply.github.com>
2024-05-20 16:30:07 +0900

    Update test_20240520.cpp

--- project/main/test_20240520.cpp
+++ project/main/test_20240520.cpp
@@ -4,7 +4,7 @@ int main() {
     int num;
     std::cout << "number:";
     std::cin >> num;
-    int squared = num * 2;
+    int squared = num * 1;
     std::cout << "that number is " << squared << " is" << std::endl;
     return 0;
 }
