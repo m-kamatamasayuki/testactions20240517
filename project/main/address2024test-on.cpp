6ec2f93596c57a6912fc34e583b953b39dfa15f4
m-kamatamasayuki <155719103+m-kamatamasayuki@users.noreply.github.com>
2024-05-28 19:38:42 +0900

    Update address2024test-on.cpp

--- project/main/address2024test-on.cpp
+++ project/main/address2024test-on.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     std::map<std::string, std::string> addressBook;
-    std::ifstream file("output-address.txt"); // ここにはあなたのファイル名を入力してください
+    std::ifstream file("*.txt"); // ここにはあなたのファイル名を入力してください
 
     if (file.is_open()) {
         std::string line;
